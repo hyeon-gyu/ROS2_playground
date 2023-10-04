e9d647de101134f038b3549099a5a6f2dbfef883
hyeongyu lim <54972659+hyeon-gyu@users.noreply.github.com>
2023-10-05 08:30:19 +0900

    Update run.sh (debug realsense camera issues)

--- dockerfile_gyu/run.sh
+++ dockerfile_gyu/run.sh
@@ -11,13 +11,13 @@ fi
 
 USER_ID=$1 ### 1000 or 1001
 TAG_NAME=$2
-REPO_NAME=ssu/misys
+REPO_NAME=awws
 IMAGE=$REPO_NAME:$TAG_NAME
 
 XSOCK=/tmp/.X11-unix
 XAUTH=$XAUTHORITY 
 VULCAN=/usr/share/vulkan
-SHARED_DOCK_DIR=/home/$USER/shared_dir
+SHARED_DOCK_DIR=/home/autoware/shared_dir
 SHARED_HOST_DIR=/home/$USER/shared_dir
 
 DEVICES="--device /dev/snd"
@@ -36,7 +36,15 @@ VOLUMES="--volume=$XSOCK:$XSOCK:ro
 	 --volume=/media:/media:rw
 	 --volume=/dev/shm:/dev/shm:rw
 	 --volume=/dev/ttyUSB0:/dev/ttyUSB0:ro
-  	 --volume=/dev/video*:/dev/video*:ro "
+	 --volume=/dev/video0:/dev/video0:rw
+	 --volume=/dev/video1:/dev/video1:rw
+	 --volume=/dev/video2:/dev/video2:rw
+	 --volume=/dev/video3:/dev/video3:rw
+	 --volume=/dev/video4:/dev/video4:rw
+	 --volume=/dev/video5:/dev/video5:rw
+	 --volume=/dev/iio/device0:/dev/iio/device0:rw
+	 --volume=/dev/iio/device1:/dev/iio/device1:rw
+	 --volume=/dev/ttyACM0:/dev/ttyACM0:rw"
 	 
 ENVIRONS="--env DISPLAY=$DISPLAY
 	  --env SDL_VIDEODRIVER=x11
@@ -70,3 +78,6 @@ docker run -it \
 	$NVIDIA \
 	$IMAGE	  
 
+
+###sudo chown -R autoware:autoware /sys/devices/platform/3610000.xhci/usb2/2-3/2-3.4/2-3.4\:1.5/0003\:8086\:0B5C.000B/HID-SENSOR-200073.2.auto/iio\:device0/
+###sudo chown -R autoware:autoware /sys/devices/platform/3610000.xhci/usb2/2-3/2-3.4/2-3.4\:1.5/0003\:8086\:0B5C.000B/HID-SENSOR-200076.3.auto/iio\:device1/
